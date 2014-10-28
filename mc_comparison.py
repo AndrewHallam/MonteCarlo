b8b45d33f5cfb37cd7b1bb3ab7a4bda46f17be88
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2014-10-28 19:24:01 +0000

    Accepts the new density if the second energy is lower or according to the second condition given in the slides.

--- mc_comparison.py
+++ mc_comparison.py
@@ -1,1 +1,13 @@
 # Compare the initial and the new energy and determine whether or not to accept the move.
+
+def compare(energy_old, energy_new)
+
+   from numpy import exp
+   from numpy.random import uniform
+
+   if energy_old > energy_new:
+      return True
+   elif exp(-(energy_new - energy_old) / 1.) > uniform():
+      return True
+   else:
+      return False
