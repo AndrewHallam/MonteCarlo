4c324cb2f4918df35249cce91fb67fa5df94ce48
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2014-10-28 18:47:59 +0000

    Started work on mc_moveparticle. A random location in the density will now be chosen.

--- mc_moveparticle.py
+++ mc_moveparticle.py
@@ -1,1 +1,14 @@
 # Move a particle left or right one space.
+
+def moveparticle(density)
+
+   from numpy import array, as array
+   from numpy.random import randint, choice
+
+   density=asarray(density)
+
+   # Choose the range over which a random particle will be selected.
+   size=density.size
+
+   # Choose the location the particle will move from.
+   loc=randint(size)
