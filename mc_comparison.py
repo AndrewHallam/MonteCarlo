0b4001c951f3164d9df6b599d5a8b4681acbe6ba
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2014-10-29 00:37:40 +0000

    Comments added to mc_comparison and mc_main.

--- mc_comparison.py
+++ mc_comparison.py
@@ -5,11 +5,13 @@ def compare(energy_old, energy_new, T):
    from numpy import exp
    from numpy.random import uniform
 
+   # Make sure the temperature is valid
    if T == 0:
       raise ValueError("Temperature must be a positive number")
    if T < 0:
       raise ValueError("Temperature must be a positive number")
 
+   # Check whether the move is accepted as defined in the notes. 
    if energy_old > energy_new:
       return True
    elif exp(-(energy_new - energy_old) / T) > uniform():
--- mc_main.py
+++ mc_main.py
@@ -1,11 +1,16 @@
 # This is the main monte carlo script which will repeat the process a number of times.
 # T is the temperature of the system.
 
-def montecarlo(density, T=100, imax=10):
+def montecarlo(density, T=100, imax=20):
 
    import diffusion_model, mc_comparison, mc_moveparticle
 
+   # Import the energy from the diffusion_model. If you wanted a different function
+   # then another function could be defined here. 
+
    energy=diffusion_model.energy(density)
+   
+   # Iterate up to imax
    for i in range(imax):
 
       density_new=mc_moveparticle.moveparticle(density)
@@ -15,8 +20,10 @@ def montecarlo(density, T=100, imax=10):
          density=density_new
          energy=energy_new
 
+      # Prints the density and energy at each step.
       print "Density is {}, and the energy is {}".format(density, energy)
 
+   # Montecarlo returns both the energy and the density since both might be handy. 
    return (energy, density)
 
-
+print montecarlo([10, 2, 1, 10, 1])
