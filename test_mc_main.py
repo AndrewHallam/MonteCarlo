efc8dcdac502abea4ca3dbf362bc60c42bd6988d
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2014-10-29 00:41:48 +0000

    Added comments to the test files.

--- test_mc_main.py
+++ test_mc_main.py
@@ -3,6 +3,7 @@ from nose.tools import assert_equal
 from numpy import sum 
 from numpy.random import random_integers
 
+# Check that the particle number is conserved over the whole process.
 def test_particle_conservation_interation():
   mctest1=random_integers(50, size=20)
   _ , mctest2=montecarlo(mctest1)
--- test_mc_moveparticle.py
+++ test_mc_moveparticle.py
@@ -1,18 +1,21 @@
-
 from mc_moveparticle import moveparticle
 from nose.tools import assert_raises, assert_equal
 from numpy import sum
 from numpy.random import random_integers
 
+# Test that zero density array rasies an error.
 def test_zero_density():
    with assert_raises(ValueError) as exception: moveparticle([0, 0, 0, 0])
-   
+
+# Test that a density array with negative values raises an error.
 def test_negative_density():
    with assert_raises(ValueError) as exception: moveparticle([-1, 1, 1, 1])
 
+# Test that a density array that isn't all integers raises an error.
 def test_nonintegers():
    with assert_raises(TypeError) as exception: moveparticle([0.1, 'a', 1])
 
+# Test that the number of particles is conserved. 
 def test_particle_conservation():
   dtest1=random_integers(50, size=20)
   dtest2=moveparticle(dtest1)
